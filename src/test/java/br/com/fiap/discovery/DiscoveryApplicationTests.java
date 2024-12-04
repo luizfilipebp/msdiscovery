a0ff160fc36ae835e7ac26733475a57b87c0bff0
luizfilipebp <luizfilipe098@gmail.com>
2024-12-04 17:31:59 -0300

    initial commit

---
+++ src/test/java/br/com/fiap/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
